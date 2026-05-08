082000e72f161b530b116862dbd2b7ac48964fd7
pablo <pablo.navane23@gmail.com>
2026-05-08 00:20:55 -0500

    Pantalla de salida de almacen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -10,6 +10,7 @@ import 'package:happy_oven/features/visualizacion_inventario/presentation/views/
 import 'package:happy_oven/features/visualizacion_inventario/presentation/views/formulario_articulo_view.dart';
 import 'package:happy_oven/features/registro_movimientos/presentation/views/historial_kardex_view.dart';
 import 'package:happy_oven/features/registro_movimientos/presentation/views/ingreso_ocr_view.dart';
+import 'package:happy_oven/features/registro_movimientos/presentation/views/salida_almacen_view.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/login',
@@ -51,6 +52,10 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/movimientos/ingreso-ocr',
       builder: (context, state) => const IngresoOcrView(),
+    ),
+    GoRoute(
+      path: '/movimientos/salida',
+      builder: (context, state) => const SalidaAlmacenView(),
     ),
   ],
 );
