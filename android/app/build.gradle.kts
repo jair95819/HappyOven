4c9ca11029e84f256940cfcc2d07829ae1dab7f8
Jeffrey Lujan <jeffreylujan27@gmail.com>
2026-05-29 14:07:40 -0500

    chore: actualizar configuracion de android y gitignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.mi_aplicacion"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "30.0.14904198"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
